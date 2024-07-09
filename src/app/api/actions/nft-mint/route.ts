856196f7d041e3539cccc12c88d6b15935b9350d
caglarGokce <93769069+caglarGokce@users.noreply.github.com>
2024-07-09 19:32:19 +0300

    fix

--- src/app/api/actions/nft-mint/route.ts
+++ src/app/api/actions/nft-mint/route.ts
@@ -86,129 +86,7 @@ export const OPTIONS = GET;
 
 export const POST = async (req: Request) => {
 
-/*
-  try {
-     const body: ActionPostRequest = await req.json();
 
-    const user = new PublicKey(body.account);
-
-
-     const connection = new Connection(clusterApiUrl("devnet"));
-
-
-
-    const mintKeypair = Keypair.generate();
-    const token_mint = mintKeypair.publicKey;
-    const decimals = 0;
-    const mintAuthority = user;
-
-
-     const metaData: TokenMetadata = {
-       updateAuthority: SystemProgram.programId,
-       mint: token_mint,
-       name: "2024 ISTANBUL",
-       symbol: "Bounty",
-       uri: "https://gist.githubusercontent.com/caglarGokce/a96891de8fef395af6943c83127a9110/raw/1d15120336665dc2bf3c0eccf04190c787a7b0f0/bountyhunt2024.json",
-       additionalMetadata: [],
-     };
-
-     // Size of Mint Account with extensions
-     const mintLen = getMintLen([ExtensionType.NonTransferable, ExtensionType.MetadataPointer]);
-     const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
-     // Size of metadata
-     const metadataLen = pack(metaData).length;
-     // Minimum lamports required for Mint Account
-     const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataExtension + metadataLen);
-
-     const createAccountInstruction = SystemProgram.createAccount({
-       fromPubkey: user, // Account that will transfer lamports to created account
-       newAccountPubkey: token_mint, // Address of the account to create
-       space: mintLen, // Amount of bytes to allocate to the created account
-       lamports, // Amount of lamports transferred to created account
-       programId: TOKEN_2022_PROGRAM_ID, // Program assigned as owner of created account
-     });
-
-     const initializeNonTransferableConfig =
-       createInitializeNonTransferableMintInstruction(
-         token_mint,
-         TOKEN_2022_PROGRAM_ID
-       );
-
-     const initializeMintInstruction = createInitializeMintInstruction(
-       token_mint, // Mint Account Address
-       decimals, // Decimals of Mint
-       mintAuthority, // Designated Mint Authority
-       mintAuthority, // Optional Freeze Authority
-       TOKEN_2022_PROGRAM_ID, // Token Extension Program ID
-     );
-
-     const initializeMetadataInstruction = createInitializeInstruction({
-       programId: TOKEN_2022_PROGRAM_ID, // Token Extension Program as Metadata Program
-       metadata: token_mint, // Account address that holds the metadata
-       updateAuthority: mintAuthority, // Authority that can update the metadata
-       mint: token_mint, // Mint Account address
-       mintAuthority: mintAuthority, // Designated Mint Authority
-       name: metaData.name,
-       symbol: metaData.symbol,
-       uri: metaData.uri,
-     });
-
-     const initializeMetadataPointerInstruction =
-       createInitializeMetadataPointerInstruction(
-         token_mint, // Mint Account address
-         SystemProgram.programId, // Authority that can set the metadata address
-         token_mint, // Account address that holds the metadata
-         TOKEN_2022_PROGRAM_ID,
-       );
-
-     const user_ata = getAssociatedTokenAddressSync(token_mint, user, false, TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
-
-     let create_ata = createAssociatedTokenAccountInstruction(user, user_ata, user, token_mint, TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
-
-     let mint_ix = createMintToCheckedInstruction(token_mint, user_ata, mintAuthority, 1, 0, [], TOKEN_2022_PROGRAM_ID);
-
-     let transfer_mint_auth = createSetAuthorityInstruction(
-       token_mint,
-       mintAuthority,
-       AuthorityType.MintTokens, // authority type
-       null,
-       [],
-       TOKEN_2022_PROGRAM_ID
-     )
-
-     const transaction = new Transaction();
-
-     transaction.add(ix);
-
-     transaction.add(createAccountInstruction);
-     transaction.add(initializeMetadataPointerInstruction)
-     transaction.add(initializeNonTransferableConfig)
-     transaction.add(initializeMintInstruction)
-     transaction.add(initializeMetadataInstruction)
-     transaction.add(create_ata)
-     transaction.add(mint_ix)
-     transaction.add(transfer_mint_auth)
-
-     transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-
-     const payload: ActionPostResponse = await createPostResponse({
-       fields: {
-         transaction
-       },
-       signers: []
-     })
-
-     return Response.json(payload, { headers: ACTIONS_CORS_HEADERS })
-   } catch (err) {
-     console.log(err);
-     let message = "An unknown error occurred";
-     if (typeof err == "string") message = err;
-     return new Response(message, {
-       status: 400,
-       headers: ACTIONS_CORS_HEADERS,
-     });
-   }
-*/
 
   try {
     const requestUrl = new URL(req.url);
@@ -231,15 +109,28 @@ export const POST = async (req: Request) => {
       process.env.SOLANA_RPC! || clusterApiUrl("devnet"),
     );
 
-    // ensure the receiving account will be rent exempt
-    const minimumBalance = await connection.getMinimumBalanceForRentExemption(
-      0, // note: simple accounts that just store native SOL have `0` bytes of data
-    );
-    if (amount * LAMPORTS_PER_SOL < minimumBalance) {
-      throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
-    }
+    const token_mint = Keypair.generate();
 
-    const transaction = getTransaction(account,toPubkey,amount)
+
+    const metaData: TokenMetadata = {
+      updateAuthority: SystemProgram.programId,
+      mint: token_mint.publicKey,
+      name: "2024 ISTANBUL",
+      symbol: "Bounty",
+      uri: "https://gist.githubusercontent.com/caglarGokce/a96891de8fef395af6943c83127a9110/raw/1d15120336665dc2bf3c0eccf04190c787a7b0f0/bountyhunt2024.json",
+      additionalMetadata: [],
+    };
+  
+    // Size of Mint Account with extensions
+    const mintLen = getMintLen([ExtensionType.NonTransferable, ExtensionType.MetadataPointer]);
+    const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
+    // Size of metadata
+    const metadataLen = pack(metaData).length;
+    // Minimum lamports required for Mint Account
+  
+    const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataExtension + metadataLen);
+
+    const transaction = getTransaction(token_mint.publicKey,account,metaData,lamports)
 
 
 
@@ -255,8 +146,8 @@ export const POST = async (req: Request) => {
         transaction,
         message: `Send ${amount} SOL to ${toPubkey.toBase58()}`,
       },
-      // note: no additional signers are needed
-      // signers: [],
+
+       signers: [token_mint],
     });
 
     return Response.json(payload, {
@@ -304,17 +195,82 @@ function validatedQueryParams(requestUrl: URL) {
   };
 }
 
-function getTransaction(account:PublicKey,toPubkey:PublicKey,amount:number){
+function getTransaction(token_mint:PublicKey,user:PublicKey,metaData:TokenMetadata,lamports:number){
 
   const tx = new Transaction();
+
+  const decimals = 0;
+  const mintAuthority = user;
   
-  tx.add(
-    SystemProgram.transfer({
-      fromPubkey: account,
-      toPubkey: toPubkey,
-      lamports: amount * LAMPORTS_PER_SOL,
-    }),
+  const mintLen = getMintLen([ExtensionType.NonTransferable, ExtensionType.MetadataPointer]);
+
+
+  const createAccountInstruction = SystemProgram.createAccount({
+    fromPubkey: user, // Account that will transfer lamports to created account
+    newAccountPubkey: token_mint, // Address of the account to create
+    space: mintLen, // Amount of bytes to allocate to the created account
+    lamports, // Amount of lamports transferred to created account
+    programId: TOKEN_2022_PROGRAM_ID, // Program assigned as owner of created account
+  });
+
+  const initializeNonTransferableConfig =
+    createInitializeNonTransferableMintInstruction(
+      token_mint,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+  const initializeMintInstruction = createInitializeMintInstruction(
+    token_mint, // Mint Account Address
+    decimals, // Decimals of Mint
+    mintAuthority, // Designated Mint Authority
+    mintAuthority, // Optional Freeze Authority
+    TOKEN_2022_PROGRAM_ID, // Token Extension Program ID
   );
+
+  const initializeMetadataInstruction = createInitializeInstruction({
+    programId: TOKEN_2022_PROGRAM_ID, // Token Extension Program as Metadata Program
+    metadata: token_mint, // Account address that holds the metadata
+    updateAuthority: mintAuthority, // Authority that can update the metadata
+    mint: token_mint, // Mint Account address
+    mintAuthority: mintAuthority, // Designated Mint Authority
+    name: metaData.name,
+    symbol: metaData.symbol,
+    uri: metaData.uri,
+  });
+
+  const initializeMetadataPointerInstruction =
+    createInitializeMetadataPointerInstruction(
+      token_mint, // Mint Account address
+      SystemProgram.programId, // Authority that can set the metadata address
+      token_mint, // Account address that holds the metadata
+      TOKEN_2022_PROGRAM_ID,
+    );
+
+  const user_ata = getAssociatedTokenAddressSync(token_mint, user, false, TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
+
+  let create_ata = createAssociatedTokenAccountInstruction(user, user_ata, user, token_mint, TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
+
+  let mint_ix = createMintToCheckedInstruction(token_mint, user_ata, mintAuthority, 1, 0, [], TOKEN_2022_PROGRAM_ID);
+
+  let transfer_mint_auth = createSetAuthorityInstruction(
+    token_mint,
+    mintAuthority,
+    AuthorityType.MintTokens, // authority type
+    null,
+    [],
+    TOKEN_2022_PROGRAM_ID
+  )
+
+  const transaction = new Transaction();
+
+  transaction.add(createAccountInstruction);
+  transaction.add(initializeMetadataPointerInstruction)
+  transaction.add(initializeNonTransferableConfig)
+  transaction.add(initializeMintInstruction)
+  transaction.add(initializeMetadataInstruction)
+  transaction.add(create_ata)
+  transaction.add(mint_ix)
+  transaction.add(transfer_mint_auth)
 
   return tx;
 }
